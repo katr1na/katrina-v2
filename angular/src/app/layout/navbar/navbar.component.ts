a4555d3ffb4fb6e2557301ee0c42f5980efe1ac8
Katrina Ngo <kngo@mines.edu>
2026-08-13 22:06:05 -0600

    feat: navbar and banner

--- angular/src/app/layout/navbar/navbar.component.ts
+++ angular/src/app/layout/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { RouterLink } from "@angular/router";
   selector: 'app-navbar',
   imports: [RouterLink],
   templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
